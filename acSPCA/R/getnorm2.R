f712dcaf57a629d7b946c251c027ded15dab17fd
Michael Anderson <52722296+and02709@users.noreply.github.com>
2024-02-28 22:28:43 -0600

    Delete acSPCA directory

--- acSPCA/R/getnorm2.R
+++
@@ -1,9 +0,0 @@
-#' L2 norm calculation
-#' @param vector vector to calculate l2 norm
-#' @keywords Sparse Adjusted Confounding Supervised Principal Component Analysis
-#' @export
-#' @examples getnorm2(vector)
-
-getnorm2 <- function(vector){
-  return(sqrt(sum(vector^2)))
-}
